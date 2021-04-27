7ee0cfd7cb193c147fdb8fcc3614b40165425c0d
kay <kayshiro@live.no>
2021-04-27 16:59:37 +0200

    Buttons done

--- app/src/main/java/com/example/mysimplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/mysimplecalculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mysimplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.mysimplecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+    // instead of using an onClickListener.
+    fun onDigit(view: View){
+        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
     }
 }
