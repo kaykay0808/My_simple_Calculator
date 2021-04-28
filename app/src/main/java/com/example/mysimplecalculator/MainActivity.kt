5c839b878b962233690dcbb0e765529dfd4a35ec
kay <kayshiro@live.no>
2021-04-28 21:53:47 +0200

    onDIgit, onClear, onDecimalPoint

--- app/src/main/java/com/example/mysimplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/mysimplecalculator/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.mysimplecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import com.example.mysimplecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric:Boolean = false
+    var lastDot:Boolean = false
 
     private lateinit var binding: ActivityMainBinding
 
@@ -17,6 +21,25 @@ class MainActivity : AppCompatActivity() {
     }
     // instead of using an onClickListener.
     fun onDigit(view: View){
-        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
+        //set text below from button
+        binding.tvInput.append((view as Button).text) //<-- this makes the texview input show the text of the button.
+        lastNumeric = true
+        
+    }
+    //clear function
+    fun onClear(view: View){
+        binding.tvInput.text =""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View){
+        // We just want to run the if Statement if the number before was a number and was not a before that dot
+        if(lastNumeric && !lastDot){
+            binding.tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
