64ca4f915eb489404cd084d65ad07ca9afb36d6c
kay <kayshiro@live.no>
2021-04-30 15:35:53 +0200

    all calculation done

--- app/src/main/java/com/example/mysimplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/mysimplecalculator/MainActivity.kt
@@ -9,8 +9,8 @@ import com.example.mysimplecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    var lastNumeric:Boolean = false
-    var lastDot:Boolean = false
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
 
     private lateinit var binding: ActivityMainBinding
 
@@ -19,8 +19,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
     // instead of using an onClickListener.
-    fun onDigit(view: View){
+    fun onDigit(view: View) {
         //Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
         //set text below from button
         binding.tvInput.append((view as Button).text) //<-- this makes the texview input show the text of the button.
@@ -29,60 +30,102 @@ class MainActivity : AppCompatActivity() {
         /*if(binding.tvInput.text.contains("1"))
             binding.tvInput.text = "Haha"*/
 
-        
+
     }
+
     //clear function
-    fun onClear(view: View){
-        binding.tvInput.text =""
+    fun onClear(view: View) {
+        binding.tvInput.text = ""
         lastNumeric = false
         lastDot = false
     }
 
-    fun onDecimalPoint(view: View){
+    fun onDecimalPoint(view: View) {
         // We just want to run the if Statement if the number before was a number and was not a before that dot
-        if(lastNumeric && !lastDot){
+        if (lastNumeric && !lastDot) {
             binding.tvInput.append(".")
             lastNumeric = false
             lastDot = true
         }
     }
 
-    fun onEqual (view: View){
-        if(lastNumeric){
+    fun onEqual(view: View) {
+        if (lastNumeric) {
             var tvValue = binding.tvInput.text.toString()
             var prefix = ""
-            try{
-                if(tvValue.startsWith("-")){
+            try {
+                if (tvValue.startsWith("-")) {
                     prefix = "-"
                     tvValue = tvValue.substring(1)
                 }
 
-                if (tvValue.contains("-")){
-
+                if (tvValue.contains("-")) {
                     // need to split String so it understand what is right and left of the operator
                     val splitValue = tvValue.split("-")
-
                     var one = splitValue[0] // 99
                     var two = splitValue[1] // 1
                     // example: 99 - 1
-
-                    if (!prefix.isEmpty()){
-                        one = prefix + 1
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
                     }
-
-
                     // THe calculation -> we convert it to Double and then to String
-                    binding.tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                    binding.tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+
+
+                }else if (tvValue.contains("/")) {
+                    // need to split String so it understand what is right and left of the operator
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0] // 99
+                    var two = splitValue[1] // 1
+                    // example: 99 - 1
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    // THe calculation -> we convert it to Double and then to String
+                    binding.tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+
+
+                }else if (tvValue.contains("+")) {
+                    // need to split String so it understand what is right and left of the operator
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0] // 99
+                    var two = splitValue[1] // 1
+                    // example: 99 - 1
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    // THe calculation -> we convert it to Double and then to String
+                    binding.tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+
+
+                }else if (tvValue.contains("*")) {
+                    // need to split String so it understand what is right and left of the operator
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0] // 99
+                    var two = splitValue[1] // 1
+                    // example: 99 - 1
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    // THe calculation -> we convert it to Double and then to String
+                    binding.tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
                 }
 
-            }catch (e: ArithmeticException){
+            } catch (e: ArithmeticException) {
                 e.printStackTrace()
             }
         }
     }
+    // This function removes second index after .0
+    fun removeZeroAfterDot(result: String) : String{
+        var value = result
+        if(result.contains(".0"))
+            value = result.substring(0, result.length - 2)
+        return value
+    }
 
-    fun onOperator(view: View){
-        if (lastNumeric && !isOperatorAdded(binding.tvInput.text.toString())){
+    fun onOperator(view: View) {
+        if (lastNumeric && !isOperatorAdded(binding.tvInput.text.toString())) {
             binding.tvInput.append((view as Button).text)
             lastNumeric = false
             lastDot = false
@@ -90,10 +133,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     // check if the operator is used
-    private fun isOperatorAdded(value: String) : Boolean{
-        return if (value.startsWith("-")){
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
             false
-        }else{
+        } else {
             value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
         }
     }
